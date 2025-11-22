1a225d6f9e659858cf112d3236f4d08a2a02fc44
Jgoioliveira <103394088+Jgoioliveira@users.noreply.github.com>
2025-11-22 17:10:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.set_page_config(
 
 SENHA_SECRETA = "baile2025"
 GOOGLE_DRIVE_FILE_ID = "1bKyxuaOkGHKkVx2e5gdYISMi7zckmyjy"
-REFRESH_INTERVAL = 300  # 5 minutos em segundos
+REFRESH_INTERVAL = 30  # 5 minutos em segundos
 
 if 'autenticado' not in st.session_state:
     st.session_state.autenticado = False
